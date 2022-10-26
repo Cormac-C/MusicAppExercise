46da9983ed3e85cebb0c9461245599908b3816b0
Cormac Cureton <cormaccureton@gmail.com>
2022-10-26 13:00:16 +0200

    Add logout button functionality

--- script/common.js
+++ script/common.js
@@ -1,5 +1,10 @@
 // * SHOULD RUN ON ALL PAGES
 
+let logout = () => {
+  localStorage.removeItem("currentUser");
+  location.reload();
+};
+
 // TODO: Figure out error
 try {
   if (localStorage.getItem("currentUser")) {
@@ -9,28 +14,35 @@ try {
       '<img src="',
       user.profilePic || "images/user.png",
       '" class=profilePic/>',
+      "<button id='logout'>",
+      "Logout",
     ].join("");
+    const button = document.getElementById("logout");
+    button.onclick = logout;
   }
 } catch (error) {
-  console.log(error)
+  console.log(error);
 }
 
 if (!localStorage.getItem("songs")) {
-  localStorage.setItem("songs", JSON.stringify([
-    {
-      "title": "Superstition",
-      "artist": "Stevie Wonder",
-      "album": "Talking Book"
-    },
-    {
-      "title": "Sir Duke",
-      "artist": "Stevie Wonder",
-      "album": "Songs in the Key of Life"
-    },
-    {
-      "title": "Signed, Sealed, Delivered (I'm Yours)",
-      "artist": "Stevie Wonder",
-      "album": "Signed, Sealed, Delivered"
-    },
-  ]));
+  localStorage.setItem(
+    "songs",
+    JSON.stringify([
+      {
+        title: "Superstition",
+        artist: "Stevie Wonder",
+        album: "Talking Book",
+      },
+      {
+        title: "Sir Duke",
+        artist: "Stevie Wonder",
+        album: "Songs in the Key of Life",
+      },
+      {
+        title: "Signed, Sealed, Delivered (I'm Yours)",
+        artist: "Stevie Wonder",
+        album: "Signed, Sealed, Delivered",
+      },
+    ])
+  );
 }
