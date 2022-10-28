c42d651f0e99f3d12bafa0f6c9dabc0a45183fe2
Cormac Cureton <cormaccureton@gmail.com>
2022-10-28 16:11:34 +0200

    Fix uploading profile picture on account create

--- script/signup.js
+++ script/signup.js
@@ -1,8 +1,22 @@
+function encodeImage(userCookie, file) {
+  var reader = new FileReader();
+  reader.onloadend = function () {
+    userCookie["profilePic"] = reader.result;
+    localStorage.setItem(userCookie.userName, JSON.stringify(userCookie));
+    localStorage.setItem("currentUser", JSON.stringify(userCookie));
+  };
+  reader.readAsDataURL(file);
+}
+
 function createCookie(event) {
   const userCookie = {};
   const formData = new FormData(event.target);
   for (var [key, value] of formData.entries()) {
-    userCookie[key] = value;
+    if (key === "profilePic") {
+      userCookie[key] = encodeImage(userCookie, value);
+    } else {
+      userCookie[key] = value;
+    }
   }
   localStorage.setItem(userCookie.userName, JSON.stringify(userCookie));
   location.href = "login.html";
